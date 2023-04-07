eae5b73cc99d37075e916a41d925b2547f0a2fb1
Muhammad Bilal Akbar <mbilalakbarkh786@gmail.com>
2023-04-07 17:48:55 +0500

    initial commit

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -5,25 +5,25 @@ import 'home_page.dart';
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  static const title = 'Shortcuts Demo';
+
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Shortcuts Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.yellow,
-        textTheme: const TextTheme(
-          bodyMedium: TextStyle(fontSize: 30),
-        ),
-        appBarTheme: const AppBarTheme(
-          centerTitle: true,
-          titleTextStyle: TextStyle(
-            fontSize: 40,
-            color: Colors.black,
-            fontWeight: FontWeight.bold,
+  Widget build(BuildContext context) => MaterialApp(
+        title: title,
+        theme: ThemeData(
+          primarySwatch: Colors.yellow,
+          textTheme: const TextTheme(
+            bodyMedium: TextStyle(fontSize: 30),
+          ),
+          appBarTheme: const AppBarTheme(
+            centerTitle: true,
+            titleTextStyle: TextStyle(
+              fontSize: 40,
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+            ),
           ),
         ),
-      ),
-      home: const HomePage(title: 'Shortcuts Demo'),
-    );
-  }
+        home: const HomePage(title: title),
+      );
 }
